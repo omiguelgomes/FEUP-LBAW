99c4ad35d8d82ff7c632f450ddfdfb14981b129d
omiguelgomes <miguelrgomes1698@gmail.com>
2020-03-03 19:08:05 +0000

    Smth

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=homePage.php" />.
+  <title>Title of the document</title>
+</head>
+
+<body>
+</body>
+
+</html>

94e7b201bb4069d3c89ad6513a30b42d7bb1d78a
omiguelgomes <miguelrgomes1698@gmail.com>
2020-03-03 19:18:54 +0000

    php fully working

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="0; url=html/homePage.php" />.
+    <meta http-equiv="refresh" content="0; url=pages/homePage.php" />.
     <title>Title of the document</title>
   </head>
   <body>

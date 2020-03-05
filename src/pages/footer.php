89c95ad7ec7fe1f156cce8bbd58260c8e6d9ddff
joao <joaopaulo_n@hotmail.com>
2020-03-05 15:22:19 +0000

    add footer

---
+++ src/pages/footer.php
@@ -0,0 +1,33 @@
+<?php function draw_footer()
+{ ?>
+    <div class="container">
+
+        <div class="row-12 py-3">
+            <nav class="navbar navbar-light bg-light">
+                <a></a>
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/uPlaceHolder.png" width="30" height="30" alt="">Phone
+                </a>
+                <a></a>
+            </nav>
+            <div class="row pl-3 pr-3">
+                <div class="card-body bg-light">
+                    <h5><a href="#" class="card-link">Contacte-nos</a> </h5>
+                    <p class="card-text">+345 925515415</p>
+                    <p class="card-text">support@uphone.com</p>
+                    <p class="card-text">R. Dr. Roberto Frias, 4200-465 Porto</p>
+                </div>
+                <div class="card-body bg-light">
+                    <h5><a href="#" class="card-link">Suporte</a> </h5>
+                    <div class="p-2 bd-highlight">
+                        <button type="button" class="btn btn-primary">F.A.Q</button>
+                    </div>
+                    <div class="ml-auto p-2 bd-highlight">
+                        <button type="button" class="btn btn-primary">Mande-nos um Ticket</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+<?php } ?>

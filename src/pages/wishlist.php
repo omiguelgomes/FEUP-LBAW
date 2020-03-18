de2ef96a2a63f35f90d48958149f9436ee396423
joao <joaopaulo_n@hotmail.com>
2020-03-18 23:58:14 +0000

    design done

--- src/pages/wishlist.php
+++ src/pages/wishlist.php
@@ -5,50 +5,132 @@ include_once('tabs.php');
 include_once('jumbo_title.php');
 include_once('footer.php');
 
-  draw_header();
-  draw_navbar();
-  draw_tabs();
-  draw_jumbo('Wishlist');
-  ?>
+draw_header();
+draw_navbar();
+draw_tabs();
+draw_jumbo('Wishlist');
+?>
+
 
 <div class="container mx-auto py-3">
-  <div class="card text-center" style="width: 18rem;">
-    <div id="carouselExampleIndicators" class="carousel slide py-3" data-ride="carousel">
-      <ol class="carousel-indicators w-25" style="filter:invert(100%); margin: auto;">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="../assets/tele1.jpg" class="d-block w-50 h-50" alt="..." style="margin: auto;">
-        </div>
-        <div class="carousel-item">
-          <img src="../assets/tele1.jpg" class="d-block w-50 h-50" alt="..." style="margin: auto;">
-        </div>
-        <div class="carousel-item">
-          <img src="../assets/tele1.jpg" class="d-block w-50 h-50" alt="..." style="margin: auto;">
+
+  <nav class="navbar navbar-light bg-light mb-4">
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </nav>
+
+
+  <div class="row">
+
+    <div class="col">
+      <div class="card text-center" style="width: 18rem;">
+        <div class="card-body">
+          <div class="container">
+            <h5 class="card-title">Samsung Galaxy S5</h5>
+            <img src="../assets/tele1.jpg" class="card-img" alt="..." style="margin: auto;">
+            <h6 class="card-text">Samsung</h6>
+            <p><b>499.99€</b></p>
+            <button type="button" class="btn btn-small btn-primary mx-2 px-3">Buy Now &nbsp;<i class="fas fa-euro-sign"></i></button>
+            <br> <br>
+            <button type="button" class="btn btn-small btn-primary mx-2">Add to Cart &nbsp;<i class="fas fa-cart-arrow-down"></i></button>
+            <p class="py-3"><a href="#" class="card-link text-danger "><b>Remove from wishlist</b></a></p>
+          </div>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev" style="filter:invert(100%)">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next" style="filter:invert(100%)">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
-    <div class="card-body">
-      <div class="container">
-        <h5 class="card-title">Samsung Galaxy S5</h5>
-        <h6 class="card-text">Samsung</h6>
-        <p><b>499.99€</b></p>
-        <button type="button" class="btn btn-small btn-primary mx-2 px-3"><i class="fas fa-euro-sign"></i></button>
-        <button type="button" class="btn btn-small btn-primary mx-2"><i class="fas fa-cart-arrow-down"></i></button>
-        <p class="py-3"><a href="#" class="card-link text-danger "><b>Remove from wishlist</b></a></p>
+
+    <div class="col">
+      <div class="card text-center" style="width: 18rem;">
+        <div class="card-body">
+          <div class="container">
+            <h5 class="card-title">Samsung Galaxy S5</h5>
+            <img src="../assets/tele1.jpg" class="card-img" alt="..." style="margin: auto;">
+            <h6 class="card-text">Samsung</h6>
+            <p><b>499.99€</b></p>
+            <button type="button" class="btn btn-small btn-primary mx-2 px-3">Buy Now &nbsp;<i class="fas fa-euro-sign"></i></button>
+            <br> <br>
+            <button type="button" class="btn btn-small btn-primary mx-2">Add to Cart &nbsp;<i class="fas fa-cart-arrow-down"></i></button>
+            <p class="py-3"><a href="#" class="card-link text-danger "><b>Remove from wishlist</b></a></p>
+          </div>
+        </div>
       </div>
     </div>
+
+    <div class="col">
+      <div class="card text-center" style="width: 18rem;">
+        <div class="card-body">
+          <div class="container">
+            <h5 class="card-title">Samsung Galaxy S5</h5>
+            <img src="../assets/tele1.jpg" class="card-img" alt="..." style="margin: auto;">
+            <h6 class="card-text">Samsung</h6>
+            <p><b>499.99€</b></p>
+            <button type="button" class="btn btn-small btn-primary mx-2 px-3">Buy Now &nbsp;<i class="fas fa-euro-sign"></i></button>
+            <br> <br>
+            <button type="button" class="btn btn-small btn-primary mx-2">Add to Cart &nbsp;<i class="fas fa-cart-arrow-down"></i></button>
+            <p class="py-3"><a href="#" class="card-link text-danger "><b>Remove from wishlist</b></a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+  <div class="row mt-4">
+
+    <div class="col">
+      <div class="card text-center" style="width: 18rem;">
+        <div class="card-body">
+          <div class="container">
+            <h5 class="card-title">Samsung Galaxy S5</h5>
+            <img src="../assets/tele1.jpg" class="card-img" alt="..." style="margin: auto;">
+            <h6 class="card-text">Samsung</h6>
+            <p><b>499.99€</b></p>
+            <button type="button" class="btn btn-small btn-primary mx-2 px-3">Buy Now &nbsp;<i class="fas fa-euro-sign"></i></button>
+            <br> <br>
+            <button type="button" class="btn btn-small btn-primary mx-2">Add to Cart &nbsp;<i class="fas fa-cart-arrow-down"></i></button>
+            <p class="py-3"><a href="#" class="card-link text-danger "><b>Remove from wishlist</b></a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card text-center" style="width: 18rem;">
+        <div class="card-body">
+          <div class="container">
+            <h5 class="card-title">Samsung Galaxy S5</h5>
+            <img src="../assets/tele1.jpg" class="card-img" alt="..." style="margin: auto;">
+            <h6 class="card-text">Samsung</h6>
+            <p><b>499.99€</b></p>
+            <button type="button" class="btn btn-small btn-primary mx-2 px-3">Buy Now &nbsp;<i class="fas fa-euro-sign"></i></button>
+            <br> <br>
+            <button type="button" class="btn btn-small btn-primary mx-2">Add to Cart &nbsp;<i class="fas fa-cart-arrow-down"></i></button>
+            <p class="py-3"><a href="#" class="card-link text-danger "><b>Remove from wishlist</b></a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card text-center" style="width: 18rem;">
+        <div class="card-body">
+          <div class="container">
+            <h5 class="card-title">Samsung Galaxy S5</h5>
+            <img src="../assets/tele1.jpg" class="card-img" alt="..." style="margin: auto;">
+            <h6 class="card-text">Samsung</h6>
+            <p><b>499.99€</b></p>
+            <button type="button" class="btn btn-small btn-primary mx-2 px-3">Buy Now &nbsp;<i class="fas fa-euro-sign"></i></button>
+            <br> <br>
+            <button type="button" class="btn btn-small btn-primary mx-2">Add to Cart &nbsp;<i class="fas fa-cart-arrow-down"></i></button>
+            <p class="py-3"><a href="#" class="card-link text-danger "><b>Remove from wishlist</b></a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </div>
 

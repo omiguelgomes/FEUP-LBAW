61c0aa4807dfd9610ddc7e993d1136f6c0efa8bc
omiguelgomes <miguelrgomes1698@gmail.com>
2020-03-25 18:05:27 +0000

    Some revisions to A5

--- src/database/create.sql
+++ src/database/create.sql
@@ -1,5 +1,5 @@
 create table if not exists users (
-	uid serial primary key,
+	id serial primary key,
 	name text not null,
 	email text unique not null,
 	birthDate date not null,
@@ -7,38 +7,38 @@ create table if not exists users (
 );
 
 create table if not exists admins (
-	uid serial primary key references users(uid) on delete cascade
+	id integer primary key references users(id) on delete cascade
 );
 
 create table if not exists faq (
-	fid serial primary key,
+	id serial primary key,
 	question text not null unique,
 	answer text not null
 );
 
 create table if not exists address (
-	aid serial primary key,
+	id serial primary key,
 	street text not null,
 	postalCode text not null,
 	country text not null,
 	city text not null,
-	uid integer not null unique references users(uid) on delete cascade
+	userID integer not null unique references users(id) on delete cascade
 );
 
 create table if not exists image(
-	iid serial primary key,
+	id serial primary key,
 	description text not null,
 	path text not null
 );
 
-create table if not exists brands(
-	mid serial primary key,
+create table if not exists brand(
+	id serial primary key,
 	"name" text not null,
-	iid integer unique not null references image(iid) on delete cascade
+	imageID integer unique not null references image(id) on delete cascade
 );
 
 create table if not exists specs(
-	esid serial primary key,
+	id serial primary key,
 	screenSize double precision not null,
 	cameraRes integer not null,
 	ram integer not null,
@@ -62,55 +62,55 @@ create table if not exists specs(
 );
 
 create table if not exists product (
-	pid serial primary key,
+	id serial primary key,
 	stock integer not null, 
 	price double precision not null, 
 	model text not null,
 	category text not null,
-	mid integer not null references brands(mid) on delete cascade,
-	esid integer not null references specs(esid) on delete cascade,
+	brandID integer not null references brand(id) on delete cascade,
+	specsID integer not null references specs(id) on delete cascade,
 	constraint stock check (stock > 0),
 	constraint price check (price > (0)::double precision),
 	constraint category check ((category = any (array['Phones'::text, 'Tablets'::text])))
 );
 
 create table if not exists eval (
-	pid serial primary key references product(pid) on delete cascade,
-	uid integer not null references users(uid) on delete cascade,
+	id integer primary key references product(id) on delete cascade,
+	userID integer not null references users(id) on delete cascade,
 	val double precision not null,
 	constraint val check (val > (0)::double precision and val <= (5)::double precision)
 );
 
 --comment and comments are reserved words... sorry
-create table if not exists coments (
-	cid serial primary key,
+create table if not exists coment (
+	id serial primary key,
 	"content" text not null,
-	pid integer not null unique references product(pid) on delete cascade
+	productID integer not null unique references product(id) on delete cascade
 );
 
 create table if not exists cart (
-	pid serial references product(pid) on delete cascade,
-	uid serial references users(uid) on delete cascade,
+	productID integer references product(id) on delete cascade,
+	userID integer references users(id) on delete cascade,
 	quant integer not null,
 	constraint quant check (quant > 0),
-	primary key(pid, uid)
+	primary key(productID, userID)
 );
 
 create table if not exists wishlist(
-	pid serial references product(pid) on delete cascade,
-	uid serial references users(uid) on delete cascade,
-	primary key(pid, uid)
+	productID integer references product(id) on delete cascade,
+	userID integer references users(id) on delete cascade,
+	primary key(productID, userID)
 );
 
 create table if not exists history(
-	pid serial references product(pid) on delete cascade,
-	uid serial references users(uid) on delete cascade,
+	productID integer references product(id) on delete cascade,
+	userID integer references users(id) on delete cascade,
 	purchaseDate date not null,
-	primary key(pid, uid)
+	primary key(productID, userID)
 );
 
 create table if not exists purchaseState(
-	eid serial primary key,
+	id serial primary key,
 	stateChangedate date not null,
 	coment text,
 	pState text not null,
@@ -118,23 +118,23 @@ create table if not exists purchaseState(
 );
 
 create table if not exists payment(
-	paid serial primary key,
+	id serial primary key,
 	"type" text not null
 	constraint "type" check (("type" = any(array['Tranferencia Bancaria'::text, 'Paypal'::text])))
 );
 
 create table if not exists purchase(
-	pid serial references product(pid) on delete cascade,
-	uid serial references users(uid) on delete cascade,
+	productID integer references product(id) on delete cascade,
+	userID integer references users(id) on delete cascade,
 	val double precision not null,
-	eid integer not null references purchaseState(eid) on delete cascade,
-	paid integer not null references payment(paid) on delete cascade,
+	statusID integer not null references purchaseState(id) on delete cascade,
+	paid integer not null references payment(id) on delete cascade,
 	constraint val check (val > (0)::double precision),
-	primary key(pid, uid)
+	primary key(productID, userID)
 );
 
 create table if not exists discount(
-	did serial primary key,
+	id serial primary key,
 	val double precision not null,
 	beginDate date not null,
 	endDate date not null,
@@ -142,14 +142,15 @@ create table if not exists discount(
 	constraint endDate check (endDate > beginDate)
 );
 
-create table if not exists disCounts(
-	pid serial references product(pid) on delete cascade,
-	did serial references discount(did) on delete cascade,
-	primary key(pid, did)
+create table if not exists discountProduct(
+	productID integer references product(id) on delete cascade,
+	discountID integer references discount(id) on delete cascade,
+	primary key(productID, discountID)
 );
 
 create table if not exists image_product(
-	pid serial references product(pid) on delete cascade,
-	iid serial references image(iid) on delete cascade,
-	primary key(pid, iid)
+	productID integer references product(id) on delete cascade,
+	imageID integer references image(id) on delete cascade,
+	primary key(productID, imageID)
 );
+

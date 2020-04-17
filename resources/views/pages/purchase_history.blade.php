c1fd526d6f8c6c4d2f2dd53f577db51a21a31953
omiguelgomes <miguelrgomes1698@gmail.com>
2020-04-17 14:12:14 +0100

    Refractored purchase_history

--- resources/views/pages/purchase_history.blade.php
+++ resources/views/pages/purchase_history.blade.php
@@ -4,58 +4,20 @@
 @include('partials.jumboTitle',['title' => 'Purchase History'])
 
 <div class="container">
-    <div class="d-flex p-3 mb-2 bg-light text-dark">
-        <div class="p-2">
-            <h4>Orders Awaiting Payment</h4>
-        </div>      
-    </div>
-    <br>
+    @foreach($products as $status => $product)
+        <div class="d-flex p-3 mb-2 bg-light text-dark">
+            <div class="p-2">
+                <h4>Orders {{$status}}</h4>
+            </div>      
+        </div>
+        <br>
 
-    <div class="table-responsive">
-        <table class="table">
-                @include('partials.phoneList',['products' => $products['Awaiting Payment'][0], 'xButton' => 'false'])
+        <div class="table-responsive">
+            <table class="table">
+                @include('partials.phoneList',['products' => $product[0], 'xButton' => 'false'])
             </tbody>
-        </table>
-    </div>
-
-    <div class="d-flex p-3 mb-2 bg-light text-dark">
-        <div class="p-2">
-            <h4>Orders Processing</h4>
-        </div>      
-    </div>
-    <br>
-
-    <div class="table-responsive">
-        <table class="table">
-                @include('partials.phoneList',['products' => $products['Processing'][0], 'xButton' => 'false'])
-            </tbody>
-        </table>
-    </div>
-
-    <div class="d-flex p-3 mb-2 bg-light text-dark">
-        <div class="p-2">
-            <h4>Orders Sent</h4>
-        </div>      
-    </div>
-    <br>
-    <div class="table-responsive">
-        <table class="table">
-                @include('partials.phoneList',['products' => $products['Sent'][0], 'xButton' => 'false'])
-            </tbody>
-        </table>
-    </div>
-
-    <div class="d-flex p-3 mb-2 bg-light text-dark">
-        <div class="p-2">
-            <h4>Orders Delivered</h4>
-        </div>      
-    </div>
-    <br>
-    <div class="table-responsive">
-        <table class="table">
-                @include('partials.phoneList',['products' => $products['Delivered'][0], 'xButton' => 'false'])
-            </tbody>
-        </table>
-    </div>
+            </table>
+        </div>
+    @endforeach
 </div>
 @endsection

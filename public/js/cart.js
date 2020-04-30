9ea7df0a4da86589cdd7a0b4acc345c03132f96c
omiguelgomes <miguelrgomes1698@gmail.com>
2020-04-30 22:40:46 +0100

    Comments on cart.js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,18 +1,19 @@
+//stuff to make ajax requests work
 $.ajaxSetup({
   headers: {
     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
   }
 });
 
-$(document).ready(function () {
-  $(document).on('click', ".remove_item", function () {
-    $.ajax({
+$(document).ready(function () { //so that these functions don't try to run before the page is loaded
+  $(document).on('click', ".remove_item", function () { //when you click element with class remove_item
+    $.ajax({ //create request
       type: "POST",
-      url: "/cart/remove/",
-      data: {
+      url: "/cart/remove/", //name of route to call
+      data: { //data given in request key: value
         id: $(this).attr('value')
       }
     })
-    $('#table_container').load('cart #product_table');
+    $('#table_container').load('cart #product_table'); //reload #product_table, put it inside of #table_container
   })
 });

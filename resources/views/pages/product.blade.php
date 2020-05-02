0a16116dec947028f4fe89ba681eda3a7bbd3479
omiguelgomes <miguelrgomes1698@gmail.com>
2020-05-02 21:12:31 +0100

    more on the phone page

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -3,97 +3,92 @@
 @section('content')
 <div class="container-fluid">
     {{-- whole page row --}}
-    <div class="row justify-content-center">
-        {{-- left side title col --}}
-        <div class="col-4">
-            <div class="jumbotron">
-                <div class="mr-auto bd-highlight">
-                    <img src="{{asset('images/'.$product->brand->image->path)}}" alt="" style="max-height: 100px;">
+    <div class="row">
+        {{-- left side phone image col --}}
+        <div class="col-5">
+            <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-interval="20000"
+                data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    @for($count = 1; $count < count($product->images); $count++)
+                        <li data-target="#carouselExampleIndicators" data-slide-to="{{$count}}"></li>
+                        @endfor
+                </ol>
+                <div class="carousel-inner pb-5 center-block text-center">
+                    <div class="carousel-item active">
+                        <img class="center-block" src="{{ asset('images/'.$product->images->first()->path) }}"
+                            alt="First Slide">
+                    </div>
+                    @for($count = 1; $count < count($product->images); $count++)
+                        <div class="carousel-item">
+                            <img class="center-block" src="{{ asset('images/'.$product->images[$count]->path) }}"
+                                alt="Slide">
+                        </div>
+                        @endfor
                 </div>
-                <h1 class="display-4"><b>{{$product->model}}</b></h1>
-                <div class="p-2 bd-highlight">
-                    <h5>
-                        @if(count($product->ratings) == 0)
-                        No ratings
-                        @else
-                        {{$product->averageRating()}}
-                        @endif
-                    </h5>
-                </div>
-                <div class="p-2 bd-highlight">
-                    <i class="fas fa-star"></i>
-                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
+
         {{-- top right side col --}}
-        <div class="col-8">
-            <div class="row">
-                {{-- phone carousel col --}}
-                <div class="col-12 mx-auto">
-                    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-interval="20000"
-                        data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            @for($count = 1; $count < count($product->images); $count++)
-                                <li data-target="#carouselExampleIndicators" data-slide-to="{{$count}}"></li>
-                                @endfor
-                        </ol>
-                        <div class="carousel-inner pb-5 center-block text-center">
-                            <div class="carousel-item active">
-                                <img class="center-block" src="{{ asset('images/'.$product->images->first()->path) }}"
-                                    alt="First Slide">
-                            </div>
-                            @for($count = 1; $count < count($product->images); $count++)
-                                <div class="carousel-item">
-                                    <img class="center-block"
-                                        src="{{ asset('images/'.$product->images[$count]->path) }}" alt="Slide">
-                                </div>
-                                @endfor
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
-                            data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
-                            data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+        <div class="col-7">
+            <div class="row h-100 align-items-center">
+                {{-- title col --}}
+                <div class="col-3">
+                    <div class="bd-highlight">
+                        <img src="{{asset('images/'.$product->brand->image->path)}}" alt="" style="max-height: 100px;">
                     </div>
                 </div>
-                {{-- phone price cart wishlist col --}}
-                <div class="col-4 px-0">
+                {{-- model col --}}
+                <div class="col-6">
+                    <h1 class="display-4"><b>{{$product->model}}</b></h1>
+                </div>
+                {{-- ratings --}}
+                <div class="col-3">
+                    <div class="p-2 bd-highlight">
+                        <h5>
+                            @if(count($product->ratings) == 0)
+                            No ratings
+                            @else
+                            {{$product->averageRating()}}
+                            @endif
+                            <i class="fas fa-star"></i>
+                        </h5>
+                    </div>
+                </div>
+                {{-- price --}}
+                <div class="col-4">
+                    <h2><b>{{$product->price}}€ </b></h2>
                     @if(count($product->discounts) > 0)
-                    <h2><b>
-                            {{$product->price}}€
-                        </b></h2>
-                    <a style="text-decoration: line-through;">
-                        <sup>{{$product->originalPrice()}}€</sup>
-                    </a>
-                    @else
-                    <h2><b>
-                            {{$product->price}}€
-                        </b></h2>
+                    <sup style="text-decoration: line-through;">{{$product->originalPrice()}}€</sup>
                     @endif
                 </div>
-                <div class="col-4 px-0">
+                {{-- cart wishlist buttons --}}
+                <div class="col-8">
                     <a href="{{ url('/product/'.$product->id.'/cart') }}" class="button btn-primary rounded p-1 mx-1">
                         Add to Cart
                     </a>
                     <img src="{{ asset('/images/shopping-cart.svg') }}" width="30" height="30" alt="cart_image">
-                </div>
-                <div class="col-4 px-0">
+
                     <a href="{{ url('/product/'.$product->id.'/wishlist') }}"
-                        class="button btn-primary rounded p-1 mx-1">
+                        class="button btn-primary rounded p-1 ml-5">
                         Add to Wishlist
                     </a>
                 </div>
-
             </div>
+
+
         </div>
+
         {{-- comments and specs --}}
-        <div class="col-10">
+        <div class="col-10 mx-auto">
             <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
                 <li class="nav-item">
                     <a class="nav-link" id="home-tab" data-toggle="tab" href="#comments" role="tab" aria-controls="home"
@@ -134,4 +129,5 @@
             </div>
         </div>
     </div>
-    @endsection
+</div>
+@endsection

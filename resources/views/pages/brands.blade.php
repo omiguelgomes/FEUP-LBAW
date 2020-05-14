0516ce85f30d273b7b2435a4e3728f51f6c5ae24
omiguelgomes <miguelrgomes1698@gmail.com>
2020-05-14 23:02:00 +0100

    cleaner brands page

--- resources/views/pages/brands.blade.php
+++ resources/views/pages/brands.blade.php
@@ -13,24 +13,19 @@
                     <div class="imgBx" data-text="Design">
                         <img src="{{'images/'.$brand->image->path}}" alt="">
                     </div>
-                    <div class="content">
-                        <div>
-                            <div class="container">
-                                <div class="row" style="height: 100%;">
-                                    @foreach($brand->products->take(5) as $product)
-                                    <div class="col-4 p-1 show-image text-right">
-                                        <img src="{{asset('images/'.$product->images->first()->path)}}"
-                                            style="width: 60%;">
-                                    </div>
-                                    @endforeach
-                                    <div class="col">
-                                        <div class="form-check">
-                                            <label class="form-check-label">
-                                                <button type="submit" class="btn btn-primary mt-4 ml-4"
-                                                    value="{{$brand->id}}" name="brand[]">See all products</button>
-                                            </label>
-                                        </div>
-                                    </div>
+                    <div class="content container">
+                        <div class="row" style="height: 100%;">
+                            @foreach($brand->products->take(5) as $product)
+                            <div class="col-4 p-1 show-image text-right">
+                                <img src="{{asset('images/'.$product->images->first()->path)}}" style="width: 60%;">
+                            </div>
+                            @endforeach
+                            <div class="col">
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <button type="submit" class="btn btn-primary mt-4 ml-4" value="{{$brand->id}}"
+                                            name="brand[]">See all products</button>
+                                    </label>
                                 </div>
                             </div>
                         </div>

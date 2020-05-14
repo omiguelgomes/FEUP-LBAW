4329ad6c007582693a8fefa5acf7e5f62aa1d8cc
omiguelgomes <miguelrgomes1698@gmail.com>
2020-05-14 23:24:06 +0100

    Small tweak in brand page

--- resources/views/pages/brands.blade.php
+++ resources/views/pages/brands.blade.php
@@ -13,18 +13,19 @@
                     <div class="imgBx" data-text="Design">
                         <img src="{{'images/'.$brand->image->path}}" alt="">
                     </div>
-                    <div class="content container">
-                        <div class="row" style="height: 100%;">
+                    <div class="content container d-flex justify-content-end ">
+                        <div class="row w-75">
                             @foreach($brand->products->take(5) as $product)
-                            <div class="col-4 p-1 show-image text-right">
-                                <img src="{{asset('images/'.$product->images->first()->path)}}" style="width: 60%;">
+                            <div class="col-4 p-1">
+                                <img class="img-fluid" src="{{asset('images/'.$product->images->first()->path)}}">
                             </div>
                             @endforeach
-                            <div class="col">
+                            <div class="col-1">
                                 <div class="form-check">
                                     <label class="form-check-label">
-                                        <button type="submit" class="btn btn-primary mt-4 ml-4" value="{{$brand->id}}"
-                                            name="brand[]">See all products</button>
+                                        <button type="submit" class="btn btn-primary mt-4 mr-4" value="{{$brand->id}}"
+                                            name="brand[]">See all
+                                            products</button>
                                     </label>
                                 </div>
                             </div>

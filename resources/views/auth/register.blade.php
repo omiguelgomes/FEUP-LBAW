14916a5358828acc6750c3b7ee6c9da56a601520
omiguelgomes <miguelrgomes1698@gmail.com>
2020-05-20 23:46:53 +0100

    Register page responsive with new look

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,93 @@
 @extends('layouts.pageWrapper')
-@section('content')  
+@section('content')
 
 @include('partials.jumboTitle',['title' => 'Register'])
 
 <div class="container">
-    <h3>Personal Information</h3>
-
-    <form method="POST" action="{{ route('register') }}">
+  <div class="card bg-primary">
+    <article class="card-body mx-auto" style="max-width: 400px;">
+      <h4 class="card-title mt-3 text-center">Create Account</h4>
+      <p class="text-center">Get started with your free account</p>
+      <form method="POST" action="{{ route('register') }}">
         {{ csrf_field() }}
         <div class="row my-1">
-    
-          <div class="form-group col-3">
-            <br><br>
-            <img src="{{ asset('/images/profilepadrao.jpg') }}" class="rounded mx-auto d-block" alt="imagempadrao" width="150" height="150">
-            <a href="#" class="">Change Photo</a>
-          </div>
           <div class="form-group col">
-            
-            <div class="row my-1">
 
-              <label for="name">Name</label>
-              <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-              @if ($errors->has('name'))
-              <span class="error">
-                {{ $errors->first('name') }}
-              </span>
-              @endif
-            </div>
-            <div class="row my-1">   
-              <label for="email">E-Mail Address</label>
-              <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required>
-              @if ($errors->has('email'))
-              <span class="error">
-                {{ $errors->first('email') }}
-              </span>
-              @endif
-            </div>
-        
-            <div class="row my-1">
-              <label for="birthdate">E-Mail Address</label>
-              <input id="birthdate" type="date" name="birthdate" value="{{ old('email') }}" required>
-              @if ($errors->has('birthdate'))
-              <span class="error">
-                {{ $errors->first('birthdate') }}
-              </span>
-              @endif
-            </div>
-            <div class="row my-1">  
-              <label for="password">Password</label>
-              <input id="password" type="password" name="password" required>
-              @if ($errors->has('password'))
-              <span class="error">
-                {{ $errors->first('password') }}
-              </span>
-              @endif
+            {{-- name  --}}
+            @if ($errors->has('name'))
+            <a class="error" style="color: red; text-align: center;">
+              {{ $errors->first('name') }}
+            </a>
+            @endif
+            <div class="form-group input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+              </div>
+              <input id="name" name="name" class="form-control" placeholder="Full name" type="text"
+                value="{{ old('name') }}" required>
             </div>
 
-            <div class="row my-1">
-              <label for="password-confirm">Confirm Password</label>
-              <input id="password-confirm" type="password" name="password_confirmation" required>
+            {{-- email --}}
+            @if ($errors->has('email'))
+            <a class="error" style="color: red; text-align: center;">
+              {{ $errors->first('email') }}
+            </a>
+            @endif
+            <div class="form-group input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+              </div>
+              <input id="email" name="email" class="form-control" placeholder="Email address" type="email"
+                value="{{ old('email') }}" required>
             </div>
-        
+
+            {{-- bithdate --}}
+            @if ($errors->has('birthdate'))
+            <a class="error" style="color: red; text-align: center;">
+              {{ $errors->first('birthdate') }}
+            </a>
+            @endif
+            <div class="form-group input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-calendar"></i> </span>
+              </div>
+              <input id="birthdate" type="date" class="form-control" name="birthdate" value="{{ old('birthdate') }}"
+                required>
+            </div>
+
+            {{-- password --}}
+            @if ($errors->has('password'))
+            <a class="error" style="color: red; text-align: center;">
+              {{ $errors->first('password') }}
+            </a>
+            @endif
+            <div class="form-group input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+              </div>
+              <input class="form-control" id="password" name="password" placeholder="Create password" type="password"
+                required>
+            </div>
+
+            {{-- repeat password --}}
+            <div class="form-group input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+              </div>
+              <input class="form-control" id="password-confirm" name="password_confirmation"
+                placeholder="Repeat password" type="password" required>
+            </div>
+
             <button type="submit">
               Register
             </button>
             <a class="button button-outline" href="{{ route('login') }}">Login</a>
+          </div>
         </div>
-    </div>
 
-    </form>
-        
-</div>
+      </form>
 
-    
-@endsection
+  </div>
+
+
+  @endsection

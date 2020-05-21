3c704432e3b78345478833ab902f11b77e44c664
omiguelgomes <miguelrgomes1698@gmail.com>
2020-05-21 23:17:30 +0100

    Added some images

--- resources/views/pages/homePage.blade.php
+++ resources/views/pages/homePage.blade.php
@@ -22,29 +22,46 @@
 </div>
 {{-- carousel --}}
 <div class="row-12 w-100">
-    <div id="carouselExampleIndicators" class="carousel slide carousel-fade new-banner" data-interval="2000"
-        data-ride="carousel">
+    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="60000">
         <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
         </ol>
-        <div class="carousel-inner">
+        <div class="carousel-inner" role="listbox">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="{{ asset('/images/teste.jpg') }}" alt="First slide">
+                <picture>
+                    {{-- image for large screens --}}
+                    <source srcset="{{asset('images/banner-wide.jpg')}}" media="(min-width: 1200px)">
+                    {{-- image for small screens --}}
+                    <source srcset="{{asset('images/banner.jpg')}}" media="(min-width: 769px)">
+                    <img srcset="{{asset('images/banner.jpg')}}" alt="responsive image"
+                        class="d-block img-fluid mx-auto">
+                </picture>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('/images/teste.jpg') }}" alt="Second slide">
+                <picture>
+                    <source srcset="{{asset('images/banner2-wide.jpg')}}" media="(min-width: 1200px)">
+                    <source srcset="{{asset('images/banner2.jpg')}}" media="(min-width: 769px)">
+                    <img srcset="{{asset('images/banner2.jpg')}}" alt="responsive image"
+                        class="d-block img-fluid mx-auto">
+                </picture>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('/images/teste.jpg') }}" alt="Third slide">
+                <picture>
+                    <source srcset="{{asset('images/banner3-wide.jpg')}}" media="(min-width: 1200px)">
+                    <source srcset="{{asset('images/banner3.jpg')}}" media="(min-width: 769px)">
+                    <img srcset="{{asset('images/banner3.jpg')}}" alt="responsive image"
+                        class="d-block img-fluid mx-auto">
+                </picture>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+
+        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
--- resources/views/partials/phoneGrid.blade.php
+++ resources/views/partials/phoneGrid.blade.php
@@ -1,26 +1,24 @@
 <div class="row mx-auto" id="phone-grid">
     @foreach($products as $product)
     <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 my-2">
-        <div class="card h-100">
+        <div class="card h-100 text-center">
             <h4 class="card-header d-flex align-items-center justify-content-around" style="height:80px;">
                 {{$product->brand->name." ".$product->model}}</h4>
-            <div class="card-body">
-                <h5 class="card-title">{{$product->brand->name}}</h5>
-                <h6 class="card-subtitle text-muted">Support card subtitle</h6>
-            </div>
-            <a href="{{url('product/'.$product->id)}}">
-                <img style="height: auto; width: 100%; display: block;"
-                    src="{{ asset('images/'.$product->images->first()->path) }}" alt="Phone image">
-            </a>
-            <div class="card-body text-center">
-                <b>
-                    <h5>{{$product->price}}€
-                        @if(count($product->discounts) > 0)
-                        <sup style="text-decoration: line-through;">{{$product->originalPrice()}}€</sup>
-                        @endif
-                    </h5>
-                </b>
-                <a type="button" class="btn btn-primary w-100" href="{{url('product/'.$product->id)}}">See</a>
+            <div class="card-body p-0">
+                <a href=" {{url('product/'.$product->id)}}">
+                    <img style="height: auto; width: 100%; display: block;"
+                        src="{{ asset('images/'.$product->images->first()->path) }}" alt="Phone image">
+                </a>
+                <div class="card-bottom px-0 pt-4">
+                    <b>
+                        <h5>{{$product->price}}€
+                            @if(count($product->discounts) > 0)
+                            <sup style="text-decoration: line-through;">{{$product->originalPrice()}}€</sup>
+                            @endif
+                        </h5>
+                    </b>
+                    <a type="button" class="btn btn-primary w-100" href="{{url('product/'.$product->id)}}">See</a>
+                </div>
             </div>
         </div>
     </div>

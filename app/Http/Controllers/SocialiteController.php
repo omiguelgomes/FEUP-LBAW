c970a6e82754ff613f78cb5bf52fb7cea8559c7a
joao <joaopaulo_n@hotmail.com>
2020-05-31 00:14:15 +0100

    up

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -27,6 +27,9 @@ class SocialiteController extends Controller
     {
         $user = Socialite::driver('google')->user();
 
+
+        echo "<h1> SEJA  BEM VINDO  {$user->getName()}</h1>";
+        echo "<img src='{$user->getAvatar()}' style='max-width: 200px; border-radius: 50%'>";
         var_dump($user);
 
         // $user->token;
